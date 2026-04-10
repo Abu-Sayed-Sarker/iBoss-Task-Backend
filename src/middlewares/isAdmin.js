cc5e9bd5ba49d79965f0952b6128ec1c8d49b5fb
Abu-Sayed-Sarker <abusayed96728@gmail.com>
2026-04-10 17:17:38 +0600

    Added test routs

--- src/middlewares/isAdmin.js
+++ src/middlewares/isAdmin.js
@@ -1,7 +1,15 @@
 import ApiError from "../utils/ApiError.js";
+import { verifyAccessToken } from "../utils/generateToken.js";
 
 const isAdmin = (req, res, next) => {
-  if (req.user && req.user.role === "admin") {
+  /// decode token and check role
+
+  const token = req.headers.authorization?.split(" ")[1];
+  const decoded = verifyAccessToken(token);
+
+  console.log(decoded.role);
+
+  if (decoded.role === "admin") {
     return next();
   }
   return next(new ApiError(403, "Access denied. Admin role required."));
